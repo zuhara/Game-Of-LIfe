143e3a30ce0b01c0103fbaf1135ecb0e78f43c7c
balabg94 <balagovind2094@gmail.com>
2020-01-02 17:19:26 +0530

    Test for the function get_neighbour() when the point comes in center(It has 8 neighbours)

--- game_of_life.py
+++ game_of_life.py
@@ -1,2 +1,11 @@
 def get_neighbours(m,p):
-    return 1
+    row = p[0]
+    col = p[1]
+    # as the point is at center 8 neighbour will be there
+    n = [m[row-1][col-1],m[row-1][col],m[row-1][col+1],m[row][col-1],m[row][col+1],m[row+1][col-1],m[row+1][col],m[row+1][col+1]]
+    t = 0
+    for i in n:
+        if i:
+            t=t+1
+    return t
+
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,6 +1,11 @@
 import game_of_life
 
-def test_neighbours_of_point_on_center():
+def test_one_neighbour_for_point_on_center():
     a = game_of_life.get_neighbours(m=[[True,False,False],[False,True,False],[False,False,False]],p = [1,1])
     e = 1
+    assert a == e
+
+def test_more_than_one_neighbours_for_point_on_center():
+    a = game_of_life.get_neighbours(m=[[True,True,True],[False,True,True],[False,False,False]],p = [1,1])
+    e = 4
     assert a == e
