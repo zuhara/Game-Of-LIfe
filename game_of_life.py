d0b3ee3bd877c1a4f680a3bd237215fa78b8886f
balabg94 <balagovind2094@gmail.com>
2020-01-03 16:53:55 +0530

    test_neighbour_for_point_on_edge

--- game_of_life.py
+++ game_of_life.py
@@ -1,11 +1,19 @@
 def get_neighbours(m,p):
-    row = p[0]
-    col = p[1]
-    # as the point is at center 8 neighbour will be there
-    n = [m[row-1][col-1],m[row-1][col],m[row-1][col+1],m[row][col-1],m[row][col+1],m[row+1][col-1],m[row+1][col],m[row+1][col+1]]
-    t = 0
+    r = p[0]
+    c = p[1]
+    n = [[r-1,c],[r+1,c],[r,c+1],[r,c-1],[r-1,c-1],[r-1,c+1],[r+1,c-1],[r+1,c+1]]
+    
+#Find the position of neighbours
+    neighbours = []
     for i in n:
-        if i:
-            t=t+1
-    return t
+        if (0 <= i[0] < len(m)) and (0 <= i[1] < len(m[0])):
+            neighbours.append([i[0],i[1]])
+            
+#Find number of live neighbours
+    live_neighbour = 0
+    for point in neighbours:
+        if m[point[0]][point[1]]:
+            live_neighbour = live_neighbour + 1           
+    return live_neighbour
+
 
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,11 +1,19 @@
 import game_of_life
 
 def test_one_neighbour_for_point_on_center():
-    a = game_of_life.get_neighbours(m=[[True,False,False],[False,True,False],[False,False,False]],p = [1,1])
+    grid = [[True,False,False],[False,True,False],[False,False,False]]
+    a = game_of_life.get_neighbours(m=grid,p = [1,1])
     e = 1
     assert a == e
 
 def test_more_than_one_neighbours_for_point_on_center():
-    a = game_of_life.get_neighbours(m=[[True,True,True],[False,True,True],[False,False,False]],p = [1,1])
+    grid = [[True,True,True],[False,True,True],[False,False,False]]
+    a = game_of_life.get_neighbours(m=grid,p = [1,1])
     e = 4
+    assert a == e
+
+def test_neighbour_for_point_on_edge():
+    grid = [[True,True,False],[True,True,False],[False,False,False]]
+    a = game_of_life.get_neighbours(m=grid,p = [2,2])
+    e = 1
     assert a == e
